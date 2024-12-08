d724e961c23763ebecc097355eb33d51dc38d2ba
Tri deptrai <bileyhas1eye@gmail.com>
2024-12-09 01:47:22 +0700

    design lan1

---
+++ CMU_PJ_CALCULATOR/CALCULATOR.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace CMU_PJ_CALCULATOR
+{
+    partial class CALCULATOR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnPHANTRAM = new Button();
+            btn8 = new Button();
+            btn5 = new Button();
+            btn2 = new Button();
+            btn0 = new Button();
+            btn7 = new Button();
+            btn4 = new Button();
+            btn1 = new Button();
+            btn3 = new Button();
+            btn6 = new Button();
+            btn9 = new Button();
+            btnCHIA = new Button();
+            btnNHAN = new Button();
+            btnTRU = new Button();
+            btnCONG = new Button();
+            btnCHAM = new Button();
+            btnXOA = new Button();
+            btnCAN = new Button();
+            btnLUYTHUA = new Button();
+            btnKETQUA = new Button();
+            txtDISPLAY = new TextBox();
+            txtHISTORY = new TextBox();
+            label1 = new Label();
+            btnCLEAR = new Button();
+            SuspendLayout();
+            // 
+            // btnPHANTRAM
+            // 
+            btnPHANTRAM.Font = new Font("Tahoma", 12F);
+            btnPHANTRAM.Location = new Point(44, 709);
+            btnPHANTRAM.Name = "btnPHANTRAM";
+            btnPHANTRAM.Size = new Size(94, 86);
+            btnPHANTRAM.TabIndex = 3;
+            btnPHANTRAM.Text = "%";
+            btnPHANTRAM.UseVisualStyleBackColor = true;
+            btnPHANTRAM.Click += btnPHANTRAM_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Tahoma", 12F);
+            btn8.Location = new Point(177, 344);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(94, 86);
+            btn8.TabIndex = 4;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Tahoma", 12F);
+            btn5.Location = new Point(177, 468);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(94, 86);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Tahoma", 12F);
+            btn2.Location = new Point(177, 592);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(94, 86);
+            btn2.TabIndex = 6;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Tahoma", 12F);
+            btn0.Location = new Point(177, 709);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(94, 86);
+            btn0.TabIndex = 7;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Tahoma", 12F);
+            btn7.Location = new Point(44, 344);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(94, 86);
+            btn7.TabIndex = 8;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Tahoma", 12F);
+            btn4.Location = new Point(44, 468);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(94, 86);
+            btn4.TabIndex = 9;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Tahoma", 12F);
+            btn1.Location = new Point(44, 592);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(94, 86);
+            btn1.TabIndex = 10;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Tahoma", 12F);
+            btn3.Location = new Point(310, 592);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(94, 86);
+            btn3.TabIndex = 18;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Tahoma", 12F);
+            btn6.Location = new Point(310, 468);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(94, 86);
+            btn6.TabIndex = 17;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Tahoma", 12F);
+            btn9.Location = new Point(310, 344);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(94, 86);
+            btn9.TabIndex = 16;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btnCHIA
+            // 
+            btnCHIA.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCHIA.Location = new Point(443, 709);
+            btnCHIA.Name = "btnCHIA";
+            btnCHIA.Size = new Size(94, 86);
+            btnCHIA.TabIndex = 15;
+            btnCHIA.Text = "➗";
+            btnCHIA.UseVisualStyleBackColor = true;
+            btnCHIA.Click += btnCHIA_Click;
+            // 
+            // btnNHAN
+            // 
+            btnNHAN.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnNHAN.Location = new Point(443, 592);
+            btnNHAN.Name = "btnNHAN";
+            btnNHAN.Size = new Size(94, 86);
+            btnNHAN.TabIndex = 14;
+            btnNHAN.Text = "✖️";
+            btnNHAN.UseVisualStyleBackColor = true;
+            btnNHAN.Click += btnNHAN_Click;
+            // 
+            // btnTRU
+            // 
+            btnTRU.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnTRU.Location = new Point(443, 468);
+            btnTRU.Name = "btnTRU";
+            btnTRU.Size = new Size(94, 86);
+            btnTRU.TabIndex = 13;
+            btnTRU.Text = "➖";
+            btnTRU.UseVisualStyleBackColor = true;
+            btnTRU.Click += btnTRU_Click;
+            // 
+            // btnCONG
+            // 
+            btnCONG.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCONG.Location = new Point(443, 344);
+            btnCONG.Name = "btnCONG";
+            btnCONG.Size = new Size(94, 86);
+            btnCONG.TabIndex = 12;
+            btnCONG.Text = "➕";
+            btnCONG.UseVisualStyleBackColor = true;
+            btnCONG.Click += btnCONG_Click;
+            // 
+            // btnCHAM
+            // 
+            btnCHAM.Font = new Font("Tahoma", 12F);
+            btnCHAM.Location = new Point(310, 709);
+            btnCHAM.Name = "btnCHAM";
+            btnCHAM.Size = new Size(94, 86);
+            btnCHAM.TabIndex = 11;
+            btnCHAM.Text = ".";
+            btnCHAM.UseVisualStyleBackColor = true;
+            btnCHAM.Click += btnCHAM_Click;
+            // 
+            // btnXOA
+            // 
+            btnXOA.Font = new Font("Tahoma", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnXOA.Location = new Point(575, 592);
+            btnXOA.Name = "btnXOA";
+            btnXOA.Size = new Size(94, 86);
+            btnXOA.TabIndex = 26;
+            btnXOA.Text = "⌫";
+            btnXOA.UseVisualStyleBackColor = true;
+            btnXOA.Click += btnXOA_Click;
+            // 
+            // btnCAN
+            // 
+            btnCAN.Font = new Font("Tahoma", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCAN.Location = new Point(575, 468);
+            btnCAN.Name = "btnCAN";
+            btnCAN.Size = new Size(94, 86);
+            btnCAN.TabIndex = 25;
+            btnCAN.Text = "√x";
+            btnCAN.UseVisualStyleBackColor = true;
+            btnCAN.Click += btnCAN_Click;
+            // 
+            // btnLUYTHUA
+            // 
+            btnLUYTHUA.Font = new Font("Tahoma", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLUYTHUA.Location = new Point(575, 344);
+            btnLUYTHUA.Name = "btnLUYTHUA";
+            btnLUYTHUA.Size = new Size(94, 86);
+            btnLUYTHUA.TabIndex = 24;
+            btnLUYTHUA.Text = "x^y";
+            btnLUYTHUA.UseVisualStyleBackColor = true;
+            btnLUYTHUA.Click += btnLUYTHUA_Click;
+            // 
+            // btnKETQUA
+            // 
+            btnKETQUA.Font = new Font("Tahoma", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnKETQUA.Location = new Point(575, 709);
+            btnKETQUA.Name = "btnKETQUA";
+            btnKETQUA.Size = new Size(94, 86);
+            btnKETQUA.TabIndex = 19;
+            btnKETQUA.Text = "=";
+            btnKETQUA.UseVisualStyleBackColor = true;
+            btnKETQUA.Click += btnKETQUA_Click;
+            // 
+            // txtDISPLAY
+            // 
+            txtDISPLAY.Enabled = false;
+            txtDISPLAY.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDISPLAY.Location = new Point(44, 50);
+            txtDISPLAY.Multiline = true;
+            txtDISPLAY.Name = "txtDISPLAY";
+            txtDISPLAY.Size = new Size(625, 200);
+            txtDISPLAY.TabIndex = 27;
+            // 
+            // txtHISTORY
+            // 
+            txtHISTORY.Enabled = false;
+            txtHISTORY.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtHISTORY.Location = new Point(715, 50);
+            txtHISTORY.Multiline = true;
+            txtHISTORY.Name = "txtHISTORY";
+            txtHISTORY.Size = new Size(167, 628);
+            txtHISTORY.TabIndex = 28;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(715, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 24);
+            label1.TabIndex = 29;
+            label1.Text = "History";
+            label1.Click += label1_Click;
+            // 
+            // btnCLEAR
+            // 
+            btnCLEAR.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCLEAR.Location = new Point(715, 709);
+            btnCLEAR.Name = "btnCLEAR";
+            btnCLEAR.Size = new Size(167, 86);
+            btnCLEAR.TabIndex = 30;
+            btnCLEAR.Text = "CLEAR";
+            btnCLEAR.UseVisualStyleBackColor = true;
+            // 
+            // CALCULATOR
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(910, 834);
+            Controls.Add(btnCLEAR);
+            Controls.Add(label1);
+            Controls.Add(txtHISTORY);
+            Controls.Add(txtDISPLAY);
+            Controls.Add(btnXOA);
+            Controls.Add(btnCAN);
+            Controls.Add(btnLUYTHUA);
+            Controls.Add(btnKETQUA);
+            Controls.Add(btn3);
+            Controls.Add(btn6);
+            Controls.Add(btn9);
+            Controls.Add(btnCHIA);
+            Controls.Add(btnNHAN);
+            Controls.Add(btnTRU);
+            Controls.Add(btnCONG);
+            Controls.Add(btnCHAM);
+            Controls.Add(btn1);
+            Controls.Add(btn4);
+            Controls.Add(btn7);
+            Controls.Add(btn0);
+            Controls.Add(btn2);
+            Controls.Add(btn5);
+            Controls.Add(btn8);
+            Controls.Add(btnPHANTRAM);
+            Name = "CALCULATOR";
+            Text = "CALCULATOR";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnPHANTRAM;
+        private Button btn8;
+        private Button btn5;
+        private Button btn2;
+        private Button btn0;
+        private Button btn7;
+        private Button btn4;
+        private Button btn1;
+        private Button btn3;
+        private Button btn6;
+        private Button btn9;
+        private Button btnCHIA;
+        private Button btnNHAN;
+        private Button btnTRU;
+        private Button btnCONG;
+        private Button btnCHAM;
+        private Button btnXOA;
+        private Button btnCAN;
+        private Button btnLUYTHUA;
+        private Button btnKETQUA;
+        private TextBox txtDISPLAY;
+        private TextBox txtHISTORY;
+        private Label label1;
+        private Button btnCLEAR;
+    }
+}
---
+++ CMU_PJ_CALCULATOR/CALCULATOR.cs
@@ -0,0 +1,117 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMU_PJ_CALCULATOR
+{
+    public partial class CALCULATOR : DevExpress.XtraEditors.XtraForm
+
+    {
+        public CALCULATOR()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPHANTRAM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCHAM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCHIA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNHAN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTRU_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCONG_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLUYTHUA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCAN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXOA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKETQUA_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
